0d32dcfae14f7397839bad1a03adbe9f7100a856
linux-jedi <cbthomas1997@gmail.com>
2016-10-30 05:13:08 -0700

    Added data analytics endpoints

--- server/server.py
+++ server/server.py
@@ -1,19 +1,72 @@
 import random
 import string
+import json
 import datetime
 
 import cherrypy
 
 from pymongo import MongoClient
 
+#import analysis
+
+@cherrypy.expose
+class BumpinessAnalytics(object):
+    def GET(self, sessionId):
+        json_data = getRideData(sessionId)
+        return bumpiness(json_data['ride_data'])
+
+def bumpiness(data):
+    TRIGGER_LEVEL = 8
+    
+    totalCount = float(len(data))
+    triggerPoints = []
+    triggerCount = 0
+
+    for i in range(1, len(data)-1):
+        if abs(data[i-1] - data[i]) > TRIGGER_LEVEL and abs(data[i+1] - data[i]) > TRIGGER_LEVEL:
+            triggerCount = triggerCount + 1
+            triggerPoints.append(i)
+        print "Trigger points: "
+        print triggerPoints
+    return triggerCount / totalCount
+
+
+# API for analytics to grab ride data
+@cherrypy.expose
+class RideAnalytics(object):
+    @cherrypy.tools.json_out()
+    def POST(self):
+        # Get most recent json ride data
+        json_data = getRideData()
+        return json_data
+
+def getRideData():
+    client = db_connect()
+    db = client.rate_your_ride
+    
+    collection = db.data
+    json_results = collection.find().limit(1).sort({'$natural':-1})
+
+    cherrypy.log(str(json_results))
+    return json_results
+
 @cherrypy.expose
 class RateYourRide(object):
     @cherrypy.tools.json_out()
     def POST(self, stringData, sessionId):
+        # Parse parameters
+        cherrypy.log("Start processing")
         cherrypy.log(sessionId)
         cherrypy.log(stringData)
+        
+        cherrypy.log("Processed data")
+        stringData = stringData[9:-2]
+        dataArray = map(int, stringData.split(','))
+        
+        cherrypy.log(sessionId)
+        print dataArray
 
-        response  = insert_ride_data(sessionId, stringData)
+        response  = insert_ride_data(sessionId, dataArray)
         return response
 
     def GET(self):
@@ -51,12 +104,12 @@ def insert_ride_data(session_id, ride_data):
 
     collection = db.data
     data_id = collection.insert_one(data).inserted_id
-    cherrypy.log(data_id)
+    cherrypy.log(str(data_id))
     
     # Update ride end time to current time
     # TODO make this its own function
     result = db.rides.update_one({'session_id': session_id}, {'$set': {'end_time': datetime.datetime.utcnow()}})
-    cherrypy.log(result)
+    cherrypy.log(str(result))
 
     # Create metrics
     metrics = create_metrics(ride_data)
@@ -114,10 +167,12 @@ if __name__ == '__main__':
     
     # Database setup
     cherrypy.engine.subscribe('start', setup_db)
-    cherrypy.engine.subscribe('stop', cleanup_db)
+    #cherrypy.engine.subscribe('stop', cleanup_db)
 
     # Start the server
     cherrypy.tree.mount(RateYourRide(), '/', conf)
+    cherrypy.tree.mount(RideAnalytics(), '/analytics', conf)
+    cherrypy.tree.mount(BumpinessAnalytics(), '/bumpiness', conf)
 
     cherrypy.engine.start()
     cherrypy.engine.block()
